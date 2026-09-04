621a29a6f8b4907c04467b733094c02347d5ded2
Raul Yulbraynner Rivera <ryrivera@uniquindio.edu.co>
2026-09-03 21:49:24 -0500

    Clase vehiculo

---
+++ Repaso/src/main/java/uniquindio/edu/co/poo/Vehiculo.java
@@ -0,0 +1,19 @@
+package uniquindio.edu.co.poo;
+
+import java.util.List;
+
+public class Vehiculo {
+
+    private String foto;
+    private String marca;
+    private String color;
+    private String tipo;
+    private String modelo;
+    private int numLlantas;
+    private int anioFabricacion;
+    private double kilometraje;
+    private int cantPasajeros;
+    private String propietarioActual;
+    private List historialPropietarios;
+
+}
